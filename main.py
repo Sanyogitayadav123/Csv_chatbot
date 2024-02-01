3726ad2989415ecfe72adf3c77b575a17abca960
sanyogita <sanyogitavkaps@gmail.com>
2024-02-01 19:02:10 +0530

    Add client image

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ def main():
         print("OPENAI_API_KEY is set")
 
     st.set_page_config(page_title="Ask your CSV")
-    st.header("Ask your CSV 📈")
+    custom_image = "./client-img.png"
+    image_width = 50
+    st.image(custom_image, width=image_width)
+    st.header("Asistent Virtual Decolandia")
+
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
     if csv_file is not None:
@@ -25,56 +29,18 @@ def main():
         agent = create_csv_agent(
             OpenAI(temperature=0), csv_file, verbose=True)
 
+
         user_question = st.text_input("Ask a question about your CSV: ")
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(agent.run(user_question))
+                language_prefix = "Răspundeți în limba română: "
+                full_question = language_prefix + user_question
+                st.write(agent.run(full_question))
 
 
 if __name__ == "__main__":
     main()
 
 
-
-# from langchain_experimental.agents import create_csv_agent
-# from langchain.llms import OpenAI
-# from dotenv import load_dotenv
-# import os
-# import streamlit as st
-
-# def main():
-#     load_dotenv()
-
-#     # Load the OpenAI API key from the environment variable
-#     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-#         print("OPENAI_API_KEY is not set")
-#         exit(1)
-#     else:
-#         print("OPENAI_API_KEY is set")
-
-#     st.set_page_config(page_title="Ask your CSV")
-#     st.header("Ask your CSV 📈")
-
-#     csv_file = st.file_uploader("Upload a CSV file", type="csv")
-#     if csv_file is not None:
-
-#         agent = create_csv_agent(
-#             OpenAI(temperature=0), csv_file, verbose=True)
-
-#         # Add a prompt template
-#         prompt_template = "Please provide information about {csv_column} in your CSV file."
-
-#         user_question = st.text_input("Ask a question about your CSV: ")
-
-#         if user_question is not None and user_question != "":
-#             # Concatenate user's question with the template
-#             full_prompt = prompt_template.format(csv_column=user_question)
-            
-#             with st.spinner(text="In progress..."):
-#                 st.write(agent.run(full_prompt))
-
-
-# if __name__ == "__main__":
-#     main()
 
